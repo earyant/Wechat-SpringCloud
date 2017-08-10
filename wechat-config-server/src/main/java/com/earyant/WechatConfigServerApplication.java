76f1fc2895df76fd606a2e6a4f1f3df82fb98ece
earyant <lirui940403@outlook.com>
2017-08-10 10:20:34 +0800

    微信公众平台配置server

---
+++ wechat-config-server/src/main/java/com/earyant/WechatConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.earyant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableConfigServer
+@SpringBootApplication
+@EnableEurekaClient
+public class WechatConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatConfigServerApplication.class, args);
+	}
+}
